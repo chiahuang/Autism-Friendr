7614ea3c3ad5db48a63ec64085b02db9813e6ca0
chiahuang <chialh@uci.edu>
2015-04-18 19:40:24 -0700

    Front end implemented
    add button and labels

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Friendr
+//
+//  Created by Kay Lab on 4/18/15.
+//  Copyright (c) 2015 TeamStringName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
